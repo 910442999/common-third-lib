4c2601d28010e8b48a6bc883b097be1a2ad8c1d6
xuanwu <910442999@qq.com>
2025-04-19 23:48:28 +0800

    -增加 tencent_cloud_chat_uikit-4.0.4

---
+++ tencent_cloud_chat_uikit/lib/business_logic/model/profile_model.dart
@@ -0,0 +1,10 @@
+import 'package:tencent_cloud_chat_sdk/models/v2_tim_conversation.dart';
+import 'package:tencent_cloud_chat_sdk/models/v2_tim_friend_info.dart';
+import 'package:tencent_cloud_chat_uikit/tencent_cloud_chat_uikit.dart';
+
+class UserProfile {
+  late V2TimFriendInfo? friendInfo;
+  late V2TimConversation? conversation;
+
+  UserProfile({required this.friendInfo, required this.conversation});
+}
---
+++ tencent_cloud_chat_uikit/lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_text_field_controller.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:tencent_cloud_chat_sdk/models/v2_tim_group_member_full_info.dart';
+
+enum ActionType {
+  hideAllPanel,
+  longPressToAt,
+  setTextField,
+  requestFocus,
+  handleAtMember
+}
+
+class TIMUIKitInputTextFieldController extends ChangeNotifier {
+  TextEditingController? textEditingController = TextEditingController();
+  ActionType? actionType;
+  String? atUserName;
+  String? atUserID;
+  String inputText = "";
+  V2TimGroupMemberFullInfo? groupMemberFullInfo;
+
+  TIMUIKitInputTextFieldController([TextEditingController? controller]) {
+    if (controller != null) {
+      textEditingController = controller;
+    }
+  }
+
+  /// text field unfocused and hide all panel
+  hideAllPanel() {
+    actionType = ActionType.hideAllPanel;
+    notifyListeners();
+  }
+
+  longPressToAt(String? userName, String? userID) {
+    actionType = ActionType.longPressToAt;
+    atUserName = userName;
+    atUserID = userID;
+    notifyListeners();
+  }
+
+  setTextField(String text) {
+    inputText = text;
+    actionType = ActionType.setTextField;
+    notifyListeners();
+  }
+
+  requestFocus() {
+    actionType = ActionType.requestFocus;
+    notifyListeners();
+  }
+
+  handleAtMember(V2TimGroupMemberFullInfo? memberInfo) {
+    actionType = ActionType.handleAtMember;
+    groupMemberFullInfo = memberInfo;
+    notifyListeners();
+  }
+}
